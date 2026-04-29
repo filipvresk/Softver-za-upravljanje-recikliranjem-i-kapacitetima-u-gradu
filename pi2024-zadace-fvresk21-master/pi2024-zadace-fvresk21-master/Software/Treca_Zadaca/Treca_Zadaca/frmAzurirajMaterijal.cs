4708a80440f92528878b30cbc11eb2a3a3e06ca5
Filip Vresk <filipvresk11@gmail.com>
2026-04-29 17:50:21 +0200

    Add files via upload

---
+++ pi2024-zadace-fvresk21-master/pi2024-zadace-fvresk21-master/Software/Treca_Zadaca/Treca_Zadaca/frmAzurirajMaterijal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Treca_Zadaca.Models;
+using Treca_Zadaca.Repozitoriji;
+
+namespace Treca_Zadaca
+{
+    public partial class frmAzurirajMaterijal : Form
+    {
+        private Materijal materijal;
+        public frmAzurirajMaterijal(Materijal materijali)
+        {
+            InitializeComponent();
+            materijal = materijali; 
+        }
+
+        private void btnExitUpdate_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmAzurirajMaterijal_Load(object sender, EventArgs e)
+        {
+            txtIdMaterialUpdate.Text = materijal.IdMaterijal.ToString();
+            txtMaterialTypeUpdate.Text = materijal.Vrsta;
+            txtMaterialPriceKgUpdate.Text=materijal.CijenapoKg.ToString();
+            txtMaterialQuantityUpdate.Text=materijal.Kolicina.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int identifikator;
+            if(int.TryParse(txtIdMaterialUpdate.Text, out identifikator))
+            {
+                materijal.IdMaterijal = identifikator;
+            }
+            materijal.Vrsta = txtMaterialTypeUpdate.Text;
+            int cijena;
+            if(int.TryParse(txtMaterialPriceKgUpdate.Text, out cijena))
+            {
+                materijal.CijenapoKg = cijena;
+            }
+            int koliko;
+            if(int.TryParse(txtMaterialQuantityUpdate.Text, out koliko))
+            {
+                materijal.Kolicina = koliko;
+            }
+            RepozitorijMaterijala.Update(materijal);
+            this.Close();
+        }
+    }
+}
---
+++ pi2024-zadace-fvresk21-master/pi2024-zadace-fvresk21-master/Software/Treca_Zadaca/Treca_Zadaca/frmDodajMaterijal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Treca_Zadaca.Models;
+using Treca_Zadaca.Repozitoriji;
+
+namespace Treca_Zadaca
+{
+    public partial class frmUnesiMaterijal : Form
+    {
+        public frmUnesiMaterijal()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInsertMaterial_Click(object sender, EventArgs e)
+        {
+            string vrsta = txtMaterialType.Text;
+            int cijenakg = int.Parse(txtMaterialPriceKg.Text);
+            int kolicina = int.Parse(txtMaterialQuantity.Text);
+
+            var materijal = new Materijal
+            {
+                
+                Vrsta = vrsta,
+                CijenapoKg = cijenakg,
+                Kolicina = kolicina
+            };
+            RepozitorijMaterijala.Add(materijal);
+            this.Close();
+        }
+
+        private void frmUnesiMaterijal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
